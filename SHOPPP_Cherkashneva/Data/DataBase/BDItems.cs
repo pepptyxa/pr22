865b8be9cc8710d83456ba1dbfb4eb58ab8574f4
user <videonet1111@gmail.com>
2025-07-22 03:51:49 +0500

    Получение данных о предметах. Черкашнева

---
+++ SHOPPP_Cherkashneva/Data/DataBase/BDItems.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data.MySqlClient;
+using SHOPPP_Cherkashneva.Data.Common;
+using SHOPPP_Cherkashneva.Data.Interfaces;
+using SHOPPP_Cherkashneva.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SHOPPP_Cherkashneva.Data.DataBase
+{
+    public class BDItems : IItems
+    {
+        public ICategorys ICategorys = new DBCategorys();
+        public IEnumerable<Items> AllItems
+        {
+            get
+            {
+                List<Items> allItems = new List<Items>();
+                MySqlConnection connection = Connection.OpenConnection();
+                MySqlDataReader dbItems = Connection.GetQuery("SELECT * FROM shop.Items ORDER BY `Name`;", connection);
+
+                while (dbItems.Read())
+                {
+                    allItems.Add(new Items()
+                    {
+                        Id = dbItems.IsDBNull(0) ? -1 : dbItems.GetInt32(0),
+                        Name = dbItems.IsDBNull(1) ? null : dbItems.GetString(1),
+                        Description = dbItems.IsDBNull(2) ? null : dbItems.GetString(2),
+                        Img = dbItems.IsDBNull(3) ? null : dbItems.GetString(3),
+                        Price = dbItems.IsDBNull(4) ? 0 : dbItems.GetInt32(4),
+                        Category = dbItems.IsDBNull(5) ? null : ICategorys.AllCategorys.Where(x => x.Id == dbItems.GetInt32(5)).First()
+                    });
+                }
+
+                Connection.CloseConnection(connection);
+
+
+                return allItems;
+            }
+        }
+    }
+}
