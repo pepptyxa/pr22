04a2b1c34f8dcbab78a6b2273b52b22ade3e7482
user <videonet1111@gmail.com>
2025-07-22 02:20:37 +0500

    Разработанное подключение к базе данных. Черкашнева.

---
+++ SHOPPP_Cherkashneva/Data/Common/Connection.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace SHOPPP_Cherkashneva.Data.Common
+{
+    public class Connection
+    {
+        readonly static string ConnectionData = "server:127.0.0.1;uid=root;pwd=;database=shop;";
+
+        public static MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(ConnectionData);
+            connection.Open();
+            return connection;
+        }
+
+        public static MySqlDataReader GetQuery (string sql, MySqlConnection connection)
+        {
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            return command.ExecuteReader();
+        }
+
+        public static void CloseConnection(MySqlConnection connection)
+        {
+            connection.Close();
+
+            MySqlConnection.ClearPool(connection); 
+        }
+
+    }
+}
